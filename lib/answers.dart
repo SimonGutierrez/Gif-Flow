90035e25ae56ced099efe24adb51dab54f1d5949
John Tieu <johnson.tieu@gmail.com>
2019-11-15 17:32:09 -0500

    main widget refactored

---
+++ lib/answers.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final String gifURL;
+  final Function answerHandler;
+  Answer(this.answerHandler,this.gifURL);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:
+        RaisedButton(
+          child: new Image.network(gifURL),
+          onPressed: answerHandler,
+          ),
+    );
+  }
+}

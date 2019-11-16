b97f8f22d771a563997551d308dc942398ac766a
SimonG <saguti15@g.holycross.edu>
2019-11-15 19:23:31 -0500

    fix: added dependencies for API requests

--- lib/result.dart
+++ lib/result.dart
@@ -1,5 +1,17 @@
+import 'package:http/http.dart' as http;
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'dart:convert';
+
+  String url = '';
+
+  Future<String> getPost() async {
+    final response = await http.get('$url/1');
+    return json.decode(response.body);
+  }
+
 class Result extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return Center(
